ffa08ca0e1d0d4500f65c56359ed9dc9fcf5e4f8
Prof Alex Etec 2021 <prof.alex.etec.2021@gmail.com>
2022-03-07 21:40:58 -0300

    Aula 07/03

---
+++ App1/App1/Data/Banco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+using App1.Models;
+
+namespace App1.Data
+{
+    public class Banco
+    {
+        readonly SQLiteAsyncConnection conexao;
+        public Banco(string arquivo)
+        {
+            conexao = new SQLiteAsyncConnection(arquivo);
+            conexao.CreateTableAsync<Computador>().Wait();
+        }
+    }
+}
